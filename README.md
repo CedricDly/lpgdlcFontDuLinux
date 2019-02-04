89ada9f866958cc9c5e302f0af3eb3f64ce37f37
CedricDly <cedric.delaunay@ensta-bretagne.org>
2019-02-04 10:55:48 +0100

    v1 du server Camera

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # lpgdlcFontDuLinux
-embsys project
+
+membres du projet : Paul BOUQUET - Cédric DELAUNAY - Thomas LE MASSON - Mathieu LOGARIO
+
+4 fevrier : premier commit du server camera. Ce dernier est capable de recevoir l'ordre de photo, puis d'envoyer une image chargée dans le dossier de l'exécutbable. Côté client python, on reçoit correctement l'image. Néanmoins, pour le moment, il faut que la taille de l'image soit connue à l'octet près.
+L'idée est qu'avant d'envoyer l'image, le server camera doit envoyer la taille de celle-ci. Pour l'instant, faire cela provoque des comportements inattendus.
+
+Prochaines étapes : 
+- modifier le code du server camera pour que lorsqu'il reçoit l'ordre de Photo, il capture la photo via v4l.
+- Signal Handler pour tous le client et les serveur
---
+++ serverCam.c
@@ -0,0 +1,102 @@
+/*
+    Socket du server Camera
+
+    compilation : gcc -o serveurCam serverCam.c (remplacer gcc par le cross-compilateur pour le faire fonctionner depuis la Rapsberry
+*/
+
+#include<stdio.h>
+#include<string.h>    
+#include<sys/socket.h>
+#include<arpa/inet.h> 
+#include<unistd.h>   
+
+#define PORT 8001
+
+
+int main(int argc , char *argv[])
+{
+    int socket_desc , client_sock , c , read_size;
+    struct sockaddr_in server , client;
+    char client_message[5 * sizeof(char)];
+
+    //Create socket
+    socket_desc = socket(AF_INET , SOCK_STREAM , 0);
+    if (socket_desc == -1)
+    {
+        printf("Could not create socket");
+    }
+    puts("Socket created");
+
+    //Prepare the sockaddr_in structure
+    server.sin_family = AF_INET;
+    server.sin_addr.s_addr = INADDR_ANY;
+    server.sin_port = htons( PORT );
+
+    //Bind
+    if( bind(socket_desc,(struct sockaddr *)&server , sizeof(server)) < 0)
+    {
+        //print the error message
+        perror("bind failed. Error");
+        return 1;
+    }
+    puts("bind done");
+
+    //Listen
+    listen(socket_desc , 3);
+
+    //Accept and incoming connection
+    puts("Waiting for incoming connections...");
+    c = sizeof(struct sockaddr_in);
+
+    //accept connection from an incoming client
+    client_sock = accept(socket_desc, (struct sockaddr *)&client, (socklen_t*)&c);
+    if (client_sock < 0)
+    {
+        perror("accept failed");
+        return 1;
+    }
+    puts("Connection accepted");
+
+    //Receive a message from client
+    while( (read_size = read(client_sock , client_message , sizeof(client_message))) > 0 )
+    {
+
+        printf("Message reçu : %s\n", client_message);
+
+        //First try, we send an image to the client
+        printf("let's send the picture to the client !\n");
+        //Get Picture Size
+        FILE *picture;
+        picture = fopen("image.jpg", "r");
+        int size;
+        //fseek(picture, 0, SEEK_END);
+        //size = ftell(picture);
+                                            /* these lines do not work with python client atm */
+        //Send Picture Size
+        //printf("Sending Picture Size\n");
+        //write(client_sock, &size, sizeof(size));
+
+        size = 1;
+        //Send Picture as Byte Array
+        printf("Sending Picture as Byte Array\n");
+        char send_buffer[size];
+        while(!feof(picture)) {
+            fread(send_buffer, 1, sizeof(send_buffer), picture);
+            write(client_sock, send_buffer, sizeof(send_buffer));
+            bzero(send_buffer, sizeof(send_buffer));
+        }
+        printf("Picture sent !\n");
+    }
+
+    if(read_size == 0)
+    {
+        puts("Client disconnected");
+        fflush(stdout);
+    }
+    else if(read_size == -1)
+    {
+        perror("recv failed");
+    }
+
+    return 0;
+}
